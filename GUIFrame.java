45406964b732ff49b643561227e99fe0030399ab
Brandon <brandonrninefive@gmail.com>
2014-10-11 17:21:01 -0400

    added frame

---
+++ GUIFrame.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+public class GUIFrame extends JFrame
+{
+	public static void main(String [] args)
+	{
+		new GUIFrame();
+	}
+	public GUIFrame()
+	{
+		setSize(500,500);
+		setResizable(false);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setTitle("Challonge Mobile Demo");
+		setVisible(true);
+	}
+}
